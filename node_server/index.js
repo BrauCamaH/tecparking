53be8367fa8ad6c4617c796a95267136c6492e52
BrauCamaH <braulio.camarenah@gmail.com>
2019-11-05 20:39:54 -0600

    Get rfi from arduino

--- node_server/index.js
+++ node_server/index.js
@@ -12,6 +12,11 @@ const app = express(); //instancia de express
 const server = http.createServer(app); //creando el server con http y express como handle request
 const io = socketio(server); //iniciando el server de socket.io
 
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('COM4', { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
+
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,15 +24,8 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.on('connection', function(socket) {
-  console.log(`client: ${socket.id}`);
-  //enviando numero aleatorio cada dos segundo al cliente
-
-  socket.emit('rfid', Math.random());
-  //recibiendo el numero aleatorio del cliente
-  socket.on('client/random', num => {
-    console.log(num);
-  });
+port.on('open', () => {
+  console.log('serial port open');
 });
 
 io.on('connection', function(socket) {
@@ -35,8 +33,18 @@ io.on('connection', function(socket) {
   socket.on('chat message', function(msg) {
     console.log('message: ' + msg);
   });
+
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  });
+
   socket.on('disconnect', function() {
     console.log('user disconnected');
+  });
+
+  parser.on('data', data => {
+    console.log('got word from arduino:', data);
+    socket.emit('rfid', data);
   });
 });
 
